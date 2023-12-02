639f720c89ffcc6e35f8141e70f3b5c35432948a
Sunil K <sktxdev@users.noreply.github.com>
2023-12-02 11:35:14 -0600

    Adds docker compose snippets

---
+++ docker-compose/mongo-compose.sh
@@ -0,0 +1,1 @@
+docker run --rm --name mongo6-jammy -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=VRuAd2Nvmp4ELHh5 -e MONGO_INITDB_DATABASE=test -v /tmp/mongo-data:/data/db mongo:6-jammy
